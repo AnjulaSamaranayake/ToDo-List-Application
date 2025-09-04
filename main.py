f6e72263706c6a6bea28e3b09525bd56d9c13c0e
AnjulaSamaranayake <anjulasankalpa2255@gmail.com>
2025-09-04 09:59:06 +0530

    Created all Mian methods

--- main.py
+++ main.py
@@ -69,6 +69,84 @@ class Todolist:
 
             print(f"{task['id']:3d}. [{status}] {task['description']}{completed_info}")
 
-            
+             # Show statistics
+            total = len(self.tasks)
+            completed = sum(1 for task in self.tasks if task['completed'])
+            pending = total - completed
+        
+            print("="*20)
+            print(f"Total: {total} | Completed: {completed} | Pending: {pending}")
+            print("="*20)
+
+        
+    # Delete a task by ID
+    def delete_task(self, task_id):
+
+        task_to_delete = None
+
+        for task in self.tasks:
+            if task['id'] == task_id:
+                task_to_delete = task
+                break
+
+        if not task_to_delete:
+            print(f"Error: Task with ID {task_id} not found.")
+            return False
+
+        # Reassign IDs to maintain sequential order
+        for i, task in enumerate(self.tasks, 1):
+            task['id'] = i
+
+        self.save_tasks()
+        print(f"Deleted task: {task_to_delete['description']}")
+        return True
     
+    # Mark a task as Completed
+    def mark_completed(self, task_id):
+        task_to_complete = None
+
+        for task in self.tasks:
+            if task['id'] == task_id:
+                task_to_complete = task
+                break
+
+        if not task_to_complete:
+            print(f"Task with ID {task_id} not Completed.")
+            return False
+
+        if task_to_complete['completed']:
+            print(f"Task '{task_to_complete['description']}' is already completed.")
+            return True
+
+        task_to_complete['completed'] = True
+        task_to_complete['completed_at'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.save_tasks()
+
+        print(f"Completed task: {task_to_complete['description']}")
+        return True
+    
+    # Mark a task as pending
+    def mark_pending(self, task_id):
+        task_to_pending = None
+
+        for task in self.tasks:
+            if task['id'] == task_id:
+                task_to_pending = task
+                break
+
+        if not task_to_pending:
+            print(f"Task with ID {task_id} not pending.")
+            return False
+        
+        if task_to_pending['pending']:
+            print(f"Task '{task_to_pending['description']}' is already peding.")
+            return True
+        
+        task_to_pending['pending'] = True
+        task_to_pending['pending_at'] = None
+        self.save_tasks()
+        print(f"Marked as pending '{task_to_pending['description']}'")
+        return True
+    
+
         
